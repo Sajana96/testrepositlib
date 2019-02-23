16b40364e5b2fa80487d84108f377d0123db1375
sajana96 <weerakonsajana@gmail.com>
2019-02-23 21:45:29 +0530

    this is a test file

---
+++ src/Codings/DBConnect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codings;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Sajana
+ */
+public class DBConnect {
+    
+    public static Connection connect(){
+        
+        Connection conn = null;
+        
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+          conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/libraryassistant","root","");
+            
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e);
+        }
+        
+        return conn;
+    }
+    
+}
